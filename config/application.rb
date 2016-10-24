951718e90e0151e5a5fd8717066afe79633871c8
calebcox725 <caleb.cox@flatironschool.com>
2016-10-24 09:10:46 -0400

    Done.

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Isncl
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.web_console.development_only = false
   end
 end
+
+
